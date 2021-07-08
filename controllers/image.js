924a57d29747d1298da02294f6ea7f01e0be8a6e
bthalpin <brian.t.halpin@gmail.com>
2021-07-08 11:16:21 -0400

    adding multiple faces

--- controllers/image.js
+++ controllers/image.js
@@ -8,18 +8,15 @@ const handleApiCall = (req,res)=>{
     app.models.predict('a403429f2ddf4b49b307e318f00e528b',
     req.body.input)
     .then(data => {
-        console.log('one',data)
         res.json(data);
-        console.log('two',data)
     })
     .catch(err => res.status(400).json('Unable to work with API'))
 }
 
 const handleImage = (req,res,db)=>{
-    const {id} = req.body;
-    console.log('three',id)
+    const {id, currentFaceCount} = req.body;
     db('users').where('id','=',id)
-    .increment('entries', 1)
+    .increment('entries', currentFaceCount)
     .returning('entries')
     .then(entries => {
         res.json(entries[0]);
